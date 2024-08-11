bdfd1dee876ed875d529047257d091d9e8a7b94f
faizan7745 <145644859+faizan7745@users.noreply.github.com>
2024-08-11 19:12:15 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+
+def addittion():
+    add = a + b
+    print(add)
+
+def subs():
+    sub = a - b
+    print(sub)
+
+def multi():
+    mult = a * b
+    print(mult)
+
+def div():
+    divi = a / b
+    print(divi)
+
+addittion()
+subs()
+multi()
+div()
